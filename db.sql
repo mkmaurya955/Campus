39827eb3aa71a44e0575358259e2d16fe078815b
manish kumar maurya <mkmaurya955@gmail.com>
2019-02-13 15:56:44 +0530

    first commit

---
+++ db.sql
@@ -0,0 +1,14 @@
+
+database name= project
+
+
+table=vacancy
+	  applications
+	  students
+	  companys
+
+
+SELECT * FROM `vacancy` company_name,job_title,salary,location,deadline,bond,age,degree_e,cpi_e,year_e,twp_e,tenp_e,job_id(primary key);
+SELECT * FROM `applications' job_id,s_mail,c_mail,status,app_id;
+SELECT * FROM `students` name,email,dob,branch,year,cpi,twp,tenp,pwd,phone,degree,id(primary);
+SELECT * FROM `companys` name,email,pwd,phone,location,ceo,cto,hr,worth,found,founder,id(primary);
